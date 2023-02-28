2671d0d69a7f1c2ed7ed06e264573d061138b4aa
Maggie Hibberd <maggiehibberd@gmail.com>
2023-02-28 20:31:16 +0000

    DB funtionality added, POST functionality and redirect once comment submitted

--- GuestBook/views.py
+++ GuestBook/views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from .import db
+from .models import Comment
 
 main = Blueprint('main', __name__)
 
@@ -11,3 +13,18 @@ def index():
 @main.route('/sign')
 def sign():
     return render_template('sign.html')
+
+
+@main.route('/sign', methods=['POST'])
+def sign_post():
+    name = request.form.get('name')
+    comment = request.form.get('comment')
+
+    new_comment = Comment(name=name, comment_text = comment)
+    db.session.add(new_comment)
+    db.session.commit()
+
+    return redirect(url_for('main.index'))
+
+
+
